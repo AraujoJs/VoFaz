d3b729fbf6e730e104687f3723f34945bf728152
Arthur Lima <mozart.bachh@gmail.com>
2023-04-21 03:07:11 +0200

    Create the main screen. Add the icon of logout in the toolbar and the fragment.
    This commit add the icon in the toolbar's menu, to exit the app.

--- app/src/main/java/com/example/vofaz/view/MainActivity.kt
+++ app/src/main/java/com/example/vofaz/view/MainActivity.kt
@@ -1,23 +1,25 @@
 package com.example.vofaz.view
 
-import android.graphics.drawable.Drawable
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
+import android.view.ContextMenu
 import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+import com.example.vofaz.Register
 import com.example.vofaz.R
+import com.example.vofaz.common.model.Database
 import com.example.vofaz.databinding.ActivityMainBinding
-import java.util.zip.Inflater
 
-class MainActivity : AppCompatActivity() {
+class MainActivity: AppCompatActivity(), Register.View {
 
+    override lateinit var presenter: Register.Presenter
     private lateinit var binding: ActivityMainBinding
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,10 +27,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val toolbar: Toolbar = binding.mainToolbar
+
+
         setSupportActionBar(toolbar)
 
         supportActionBar?.title = ""
-
 
         with(binding) {
 
@@ -37,5 +40,55 @@ class MainActivity : AppCompatActivity() {
 
 
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.menu_logout -> {
+                goToLoginScreen()
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun showProgress(enabled: Boolean) {
+        TODO("Not yet implemented")
+    }
+
+    override fun displayNameFailure(nameError: Int?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun displayEmailFailure(emailError: Int?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun displayPasswordFailure(passwordError: Int?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun displayPasswordNotEquals(notEqualsError: Int?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onUserNotCreated(message: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onUserCreated() {
+        TODO("Not yet implemented")
+    }
+
+    private fun goToLoginScreen() {
+        val intent = Intent(this, LoginActivity::class.java)
+        Database.sessionAuth = null
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(intent)
     }
 }
